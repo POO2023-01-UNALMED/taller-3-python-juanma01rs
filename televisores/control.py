754c55780a178b901ad189013c7be61dd3476f3b
juanma01rs <juanma10rs@gmail.com>
2023-02-27 14:45:11 -0500

    Python 3

--- televisores/control.py
+++ televisores/control.py
@@ -1,22 +1,35 @@
 class Control:
-    import tv
-    tv.turnOn()
-    tv.turnOff()
-    tv.canalUp()
-    tv.canalDown()
-    tv.volumenUp()
-    tv.volumenDown()
-    tv.setCanal()
-    
+   
     def __init__(self):
         self._tv = None
         
-    def enlazar (self, televisor):
-        self._tv = televisor
-        self.tv._control = self
+    def turnOn(self):
+        self.tv.turnOn()
+        
+    def turnOff(self):
+        self.tv.turnOff()
     
-    def setTv (self, televisor):
-        self._tv = televisor
+    def canalUp(self):
+        self.tv.canalUp()
+        
+    def canalDown(self):
+        self.tv.canalDown()
+    
+    def volumentUp(self):
+        self.tv.volumentUp()
+        
+    def volumentDown(self):
+        self.tv.volumentDown()
+                   
+    def setCanal(self, canal):
+        self.tv.setCanal(canal)            
+        
+    def enlazar (self, tv):
+        self._tv = tv
+        tv.setControl(self)
+    
+    def setTv (self, tv):
+        self._tv = tv
     
     def getTv (self):
         return self._tv
